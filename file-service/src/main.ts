ae4167ad7ae0beb688ea6aa5ce88ff7cf7f9a739
Bartłomiej <bartlomiej.kotarski@netguru.com>
2021-10-31 11:42:30 +0100

    :sparkles: init project with config module

---
+++ file-service/src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { EnvConfigService } from './env-config/services/env-config.service';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(EnvConfigService);
+
+  await app.listen(configService.getAppConfig().APP_PORT);
+}
+bootstrap();
---
+++ user-service/src/env-config/models/config-env.model.ts
@@ -0,0 +1,15 @@
+import { Type } from 'class-transformer';
+import { IsEnum, IsInt, IsString } from 'class-validator';
+import { EnvTypeEnum } from '../types/env-type.enum';
+
+export class ConfigEnvModel {
+  @IsEnum(EnvTypeEnum)
+  NODE_ENV: EnvTypeEnum;
+
+  @IsInt()
+  @Type(() => Number)
+  APP_PORT: number;
+
+  @IsString()
+  APP_HOST: string;
+}
