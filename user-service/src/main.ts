5f811bfc127a28ae185b57945d2ce502adeda9ee
Bartłomiej <bartlomiej.kotarski@netguru.com>
2021-11-14 12:18:24 +0100

    :sparkles: add docs for user-service

--- user-service/src/main.ts
+++ user-service/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 import { EnvConfigService } from './env-config/services/env-config.service';
 import { Transport } from '@nestjs/microservices';
 import { RmqOptions } from '@nestjs/microservices/interfaces/microservice-configuration.interface';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -19,6 +21,17 @@ async function bootstrap() {
       },
     },
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('User service')
+    .setDescription('The "user service" for store app users')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true, forbidNonWhitelisted: true }));
 
   await microservice.listen();
   await app.listen(configService.getAppConfig().APP_PORT);
--- user-service/src/user/controllers/user.controller.ts
+++ user-service/src/user/controllers/user.controller.ts
@@ -1,15 +1,16 @@
 import { Controller, Get, Req } from '@nestjs/common';
-import { UserService } from '../services/user.service';
 import { UserRequest } from '../../common/interfaces/user-req.interface';
 import { Authorized } from '../../auth/decorators/authorized.decorator';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { UserAuth } from '../../common/types/user-auth.type';
 
 @Controller('/users')
+@ApiTags('users')
+@ApiBearerAuth()
 export class UserController {
-  constructor(private readonly userService: UserService) {}
-
   @Get('/')
   @Authorized()
-  public async getLoggedUserInf(@Req() req: UserRequest): Promise<any> {
+  public async getLoggedUserInf(@Req() req: UserRequest): Promise<UserAuth> {
     return req.user;
   }
 }
